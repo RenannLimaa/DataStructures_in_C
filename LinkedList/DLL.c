12d7a8118f41c754eb611ee0fc477c023917d3ff
RenannLimaa <renanrod2403@gmail.com>
2023-12-30 11:38:34 -0300

    add insertAtEnd function

--- LinkedList/DLL.c
+++ LinkedList/DLL.c
@@ -11,13 +11,65 @@ typedef struct Node {
 Node* createNode() 
 {
 	Node *n = (Node *)malloc(sizeof(Node));
-	n->next = NULL;
-	n->next = NULL;
+   // n->data = value;
+    n->next = NULL;
+	n->prev = NULL;
 
 	return n;
 }
+
+void insertAtEnd(Node **head, int value)
+{
+    Node *newNode = createNode();
+    newNode->data = value;
+    newNode->next = NULL;
+        
+    if (*head == NULL) {
+        *head = newNode;
+        newNode->prev = NULL;
+    } 
+    
+    else {
+        Node *tmp = *head;
+        while(tmp->next != NULL) {
+            tmp = tmp->next;
+        }
+
+        tmp->next = newNode;
+        newNode->prev = tmp;
+    }
+}
+
+void deleteList(Node **head) {
+    Node *tmp;
+    while (*head != NULL) {
+        tmp = *head;
+        *head = (*head)->next;
+        free(tmp);
+    }
+}
+
+void printList(Node **head)
+{
+    if (*head == NULL)
+        printf("Empty list!\n");
+    
+    Node *tmp = *head;
+    while(tmp != NULL) {
+        printf("%d\n", tmp->data);
+        tmp = tmp->next;
+    }
+
+}
+
 int main() 
 {
-	Node *head = createNode();
+	Node *head;
+
+    insertAtEnd(&head, 10);
+
+    printList(&head);
+    
+    deleteList(&head);
 	return 0;
 }
