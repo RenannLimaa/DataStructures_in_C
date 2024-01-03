382f2c383bc0e8a187be9eb65b81634b3000baf3
RenannLimaa <renanrod2403@gmail.com>
2024-01-02 22:09:36 -0300

    add circularDLL program

---
+++ LinkedList/circularDLL.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *prev;
+    struct Node *next;
+} Node;
+
+Node* createNode(int value) 
+{
+    Node *n = (Node *)malloc(sizeof(Node));
+    n->data = value;
+    n->next = n;
+    n->prev = NULL;
+
+    return n;
+}
+
+void insertAtEnd(Node **head, int value)
+{
+    Node *newNode = createNode(value);
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    Node *tmp = *head;
+    while (tmp->next != *head) {
+        tmp = tmp->next;
+    }
+
+    tmp->next = newNode;
+
+    newNode->next = *head;
+    newNode->prev = tmp;
+}
+
+void deleteList(Node **head)
+{
+    if (*head == NULL) {
+        printf("deleteList: empty list!\n");
+        return;
+    }
+
+    Node *tmp, *current = *head;
+    do {
+        tmp = current;
+        current = current->next;
+        free(tmp);
+    } while (current != *head);
+}
+
+void printList(Node **head) 
+{
+    if (*head == NULL) {
+        printf("printList: empty list!\n");
+        return;
+    }
+
+    Node *tmp = *head;
+    do {
+        printf("%d\n", tmp->data);
+        tmp = tmp->next;
+    } while (tmp != *head);
+}
+int main() 
+{
+    Node *head;
+
+    insertAtEnd(&head, 1);
+    insertAtEnd(&head, 2);
+    insertAtEnd(&head, 3);
+    printList(&head);
+    deleteList(&head);
+}
