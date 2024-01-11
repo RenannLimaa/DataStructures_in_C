ee4402931628aac8ec55c7e4c49536f9ba9fe5a8
RenannLimaa <renanrod2403@gmail.com>
2024-01-11 18:58:40 -0300

    WIP: add insert functions

--- Tree/tree.c
+++ Tree/tree.c
@@ -3,15 +3,52 @@
 
 typedef struct Node {
     int data;
-    struct Node *rChild;
-    struct Node *lChild;
-} Node 
+    struct Node *right;
+    struct Node *left;
+} Node;
 
 Node* createNode(int value) 
 {
     Node *n = (Node *)malloc(sizeof(Node));
-    n->rChild = NULL;
-    n->lChild = NULL;
+    n->data  = value;
+    n->right = NULL;
+    n->left  = NULL;
 
     return n;
+}
+
+void insertAtLeft(Node **node, int el)
+{
+    if ((*node)->left == NULL) {
+        Node *newNode = createNode(el);
+        (*node)->left = newNode;
+    }
+
+    else {
+        if (el < (*node)->left->data)
+            insertAtLeft((*node)->left, el);
+        else 
+            insertAtRight((*node)->left, el);
+    }
+}
+
+
+void insert(Node **root, int el) 
+{
+    Node *newNode = createNode(el);
+
+    if (*root == NULL) 
+        *root = newNode;
+
+    else {
+        if (el > (*root)->data) 
+            (*root)->right = newNode;
+        else 
+            (*root)->left  = newNode;
+    }
+}
+
+int main() 
+{
+    return 0;
 }
